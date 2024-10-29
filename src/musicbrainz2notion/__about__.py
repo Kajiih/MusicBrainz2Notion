8134dab2967be0a3037724d64884ee82a71b8333
Julian <julian_p@snu.ac.kr>
2024-10-29 03:48:49 +0100

    🔖 0.3.0 tag

--- src/musicbrainz2notion/__about__.py
+++ src/musicbrainz2notion/__about__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 
 __app_name__ = "musicbrainz2notion"
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 __author__ = "Kajih"
 __author_email__ = "itskajih@gmail.com"
 __repo_url__ = "https://github.com/Kajiih/musicbrainz2notion"
